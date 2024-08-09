21758c2072daa219981814cda02586b497bfa145
samuel kyama <samuelkyama102@gmail.com>
2024-08-09 21:45:34 +0300

    connect db

---
+++ db.js
@@ -0,0 +1,22 @@
+// db.js
+require("dotenv").config();
+const mysql = require("mysql2");
+
+const connection = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+  port: process.env.DB_PORT,
+  connectTimeout: 1000000,
+});
+
+connection.connect((err) => {
+  if (err) {
+    console.error("Error connecting to the database:", err.stack);
+    return;
+  }
+  console.log("Connected to the database as ID", connection.threadId);
+});
+
+module.exports = connection;
--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-// index.js
+const db = require("./db");
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
@@ -6,6 +6,18 @@ const port = process.env.PORT || 3000;
 app.get("/", (req, res) => {
   res.send("Hello, World! Tessst");
 });
+
+// Example query to test the connection
+db.query("SELECT * FROM users", (err, results, fields) => {
+  if (err) {
+    console.error("Error executing query:", err.stack);
+    return;
+  }
+  console.log("Query results:", results);
+});
+
+// Close the connection when done
+db.end();
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
